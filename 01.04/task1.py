d51b444be908ed9a02d81a3822d5dd279bb32cf7
Denis1325 <penkovdenis271@gmail.com>
2026-04-01 03:10:37 +0300

    first solution

--- 01.04/task1.py
+++ 01.04/task1.py
@@ -1,1 +1,18 @@
+import sys
+import heapq
 
+numbers = []
+for i in range(int(input())):
+    numbers.append(int(input()))
+
+heapq.heapify(numbers)
+time = 0
+
+while len(numbers) > 1:
+    a = heapq.heappop(numbers)
+    b = heapq.heappop(numbers)
+    s = a + b
+    time += s
+    heapq.heappush(numbers, s)
+
+print(time)
