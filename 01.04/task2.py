f9fbe2cf3a00c6c36a83dd425f408072a3ed1809
Denis1325 <penkovdenis271@gmail.com>
2026-04-01 03:46:37 +0300

    solution

--- 01.04/task2.py
+++ 01.04/task2.py
@@ -1,1 +1,18 @@
+import heapq
 
+numbers = []
+n = int(input())
+for i in range(n):
+    numbers.append(int(input()))
+k = int(input())
+
+heap = []
+result = []
+for i in range(len(numbers)):
+    value = numbers[i]
+    heapq.heappush(heap, (-value, i))          # -value, т.к. heapq использует min-heap
+    if i >= k - 1:
+        while heap and heap[0][1] <= i - k:
+            heapq.heappop(heap)
+        result.append(str(-heap[0][0]))
+print(' '.join(result))
